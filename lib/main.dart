ab35206cc17ce596f45a5dda2edbfc383e98e38f
Calimlim <jhonrneilebuan@gmail.com>
2024-07-30 19:06:28 +0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Country List',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: HomeScreen(),
     );
   }
